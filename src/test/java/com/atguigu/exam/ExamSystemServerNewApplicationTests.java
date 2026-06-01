885694858a3f3814ec06effbb85e33b2cb9b07b9
JinTongXue-git <coc3209265610@gmail.com>
2026-06-01 21:34:10 +0800

    chore(build): 引入的依赖本项目开发的依赖

---
+++ src/test/java/com/atguigu/exam/ExamSystemServerNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSystemServerNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
